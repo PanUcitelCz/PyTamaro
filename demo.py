ecbe7e2d52253c795e83eee153a41faf5e084fd7
panucitelcz <petr.b257@seznam.cz>
2024-10-04 09:53:04 +0200

    first commit

---
+++ demo.py
@@ -0,0 +1,8 @@
+from pytamaro import rectangle, rotate, overlay, green, show_graphic, red, white
+
+red_background = rectangle(320,320,red)
+
+field = rectangle(100, 60, green)
+show_graphic(field)
+
+show_graphic(field)
